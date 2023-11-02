c2615b5080d250ed3f68fedaf7a01e09aaa4a8cd
FatimaRustamova <frustemova34@gmail.com>
2023-11-02 14:19:06 +0400

    Taskk18

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let Nail_ceki1 = 92;
 let Nihad_boy1 = 169;
 let Nail_boy1 = 195;
 
-let Nihad_BKI1 = Nihad_ceki1 / (Nihad_boy1 ** 2);
-let Nail_BKI1 = Nail_ceki1 / (Nail_boy1 ** 2);
+Nihad_BKI1 = Nihad_ceki1 / (Nihad_boy1 ** 2);
+Nail_BKI1 = Nail_ceki1 / (Nail_boy1 ** 2);
 
 console.log(Nihad_BKI1);
 console.log(Nail_BKI1);
@@ -23,8 +23,8 @@ let Nail_ceki2 = 85;
 let Nihad_boy2 = 188;
 let Nail_boy2 = 176;
 
-let Nihad_BKI2 = Nihad_ceki2 / (Nihad_boy1 ** 2);
-let Nail_BKI2 = Nail_ceki2 / (Nail_boy1 ** 2);
+Nihad_BKI2 = Nihad_ceki2 / (Nihad_boy2 ** 2);
+Nail_BKI2 = Nail_ceki2 / (Nail_boy2 ** 2);
 
 console.log(Nihad_BKI2);
 console.log(Nail_BKI2);
