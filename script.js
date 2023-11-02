28baef50edab27be74f0bc21d0026f9362c19ac5
FatimaRustamova <frustemova34@gmail.com>
2023-11-02 11:41:20 +0400

    Javascript

---
+++ script.js
@@ -0,0 +1,37 @@
+//Data1.
+let Nihad_ceki1 = 78;
+let Nail_ceki1 = 92;
+let Nihad_boy1 = 169;
+let Nail_boy1 = 195;
+
+let Nihad_BKI1 = Nihad_ceki1 / (Nihad_boy1 ** 2);
+let Nail_BKI1 = Nail_ceki1 / (Nail_boy1 ** 2);
+
+console.log(Nihad_BKI1);
+console.log(Nail_BKI1);
+
+if(Nihad_BKI1 > Nail_BKI1){
+    console.log(Boolean (true));
+}
+else{
+    console.log(Boolean (false));
+}
+
+//Data2.
+let Nihad_ceki2 = 95;
+let Nail_ceki2 = 85;
+let Nihad_boy2 = 188;
+let Nail_boy2 = 176;
+
+let Nihad_BKI2 = Nihad_ceki2 / (Nihad_boy1 ** 2);
+let Nail_BKI2 = Nail_ceki2 / (Nail_boy1 ** 2);
+
+console.log(Nihad_BKI2);
+console.log(Nail_BKI2);
+
+if(Nihad_BKI2 > Nail_BKI2){
+    console.log(Boolean (true));
+}
+else{
+    console.log(Boolean (false));
+}
